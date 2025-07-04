3aee40ddcecbfe69f1fa8fab2ec23995bb1a1a1d
mirzohid777 <mirzohidshermanov@gmail.com>
2025-07-04 16:54:24 +0300

    Add files via upload

---
+++ mean.js
@@ -0,0 +1,25 @@
+
+let a = 5;
+let b = 3;
+console.log(a + b)
+
+
+const name = prompt ("ISMINGIZ KIRITING")
+const surname =prompt ("FAMILYANGIZNI KIRITING")
+const age = prompt ("YOSHINGIZ KIRITING")
+const city = prompt("YASHASH JOYINGIZ KIRITING")
+const subject =prompt("QAYSI KURSGA YOZILMOQCHISIZ")
+alert(`MALUMOT KIRITILDI`)
+alert(`SIZ BILAN APERATORLARIMIZ BOG'LANAD`)
+
+
+
+
+
+// const name = prompt("Ismingizni kiriting");
+// const age = prompt("Yoshingizni kiriting");
+// const city = prompt("Shaharni kiriting");
+// alert(malulot kiritildi)
+// console.log(Ism: ${name});
+// console.log(Age: ${age});
+// console.log(City: ${city});
